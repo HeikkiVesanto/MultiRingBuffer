bf137953af52fca446d31ff40e8e6f9645608776
Heikki Vesanto <heikki.vesanto@gmail.com>
2018-01-23 21:48:59 +0000

    Updated for QGIS 3

---
+++ multi_ring_buffer_dialog_base.ui.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'multi_ring_buffer_dialog_base.ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
